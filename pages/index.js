22de8c9e73a89a167c548f6a1a1e9aa8b080e5e5
Mun94 <seongwoo720@gmail.com>
2021-04-25 15:37:48 +0900

    add: dynamic routes, next/link

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,36 @@
+import { useEffect, useState } from "react";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
+import axios from "axios";
+import ItemList from "../src/component/ItemList";
+import { Divider, Header } from "semantic-ui-react";
 
 export default function Home() {
+  const [list, setList] = useState([]);
+  const API_URL =
+    "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline";
+
+  function getData() {
+    axios.get(API_URL).then((res) => {
+      setList(res.data);
+    });
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div>
       <Head>
         <title>home | 문성우</title>
       </Head>
-      create-next-app으로 설치하면
-      <br />
-      1. 컴파일과 번들링이 자동으로 된다.(webpack과 babel)
-      <br />
-      2. 자동 리프레쉬 기능으로 수정하면 화면에 바로 반영된다.
-      <br />
-      3. 서버사이드 렌더링이 지원된다.
-      <br />
-      4. 스태틱 파일을 지원한다.
+      <Header>베스트 상품</Header>
+      <Divider />
+      <ItemList list={list.slice(0, 9)} />
+      <Header>신상품</Header>
+      <Divider />
+      <ItemList list={list.slice(9)} />
     </div>
   );
 }
